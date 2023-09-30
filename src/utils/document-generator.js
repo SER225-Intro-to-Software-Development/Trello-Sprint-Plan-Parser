9ed060ec6ab5450bacb0a190c15ff6c67d6cdadc
a-r-t <alexthimineur@gmail.com>
2023-09-30 17:42:48 -0400

    creating document file successful

--- src/utils/document-generator.js
+++ src/utils/document-generator.js
@@ -2,23 +2,21 @@ const { Document, HeadingLevel, Packer, Paragraph, TextRun } = require('docx')
 const { saveAs } = require('file-saver')
 
 const generateDocumentForSprintPlan = (trelloJson, sprintPlanColumnName) => {
-  console.log(trelloJson.actions)
   const lists = trelloJson.lists
-  const sprintPlanList = lists.find(list => list.name === sprintPlanColumnName)
+  const sprintPlanList = lists.find(list => stripString(list.name) === stripString(sprintPlanColumnName))
   if (sprintPlanList) {
     const id = sprintPlanList.id
     const cards = trelloJson.cards
     const cardsInSprintPlanList = cards.filter(card => card.idList === id).map(card => formatCard(card, trelloJson))
-    console.log(cardsInSprintPlanList)
     const document = createDocument(cardsInSprintPlanList)
     return Packer.toBlob(document)
       .then(blob => {
-        console.log(blob)
         saveAs(blob, "example.docx");
-        console.log("Document created successfully");
       })
   }
 }
+
+const stripString = (str) => str.trim().toLowerCase().replace(/\s/g, '')
 
 const formatCard = (card, trelloJson) => {
   return {
@@ -46,20 +44,95 @@ const createDocument = (cardsInSprintPlanList) => {
   return new Document({
     sections: [{
       properties: {},
-      children: cardsInSprintPlanList.map(card => createCardDocumentSection(card))
+      children: cardsInSprintPlanList.map(card => createCardDocumentSection(card)).flat()
     }]
   })
 }
 
 const createCardDocumentSection = (card) => {
-  return new Paragraph({
-    children: [
-      new TextRun({
-        text: card.title + '\n\n',
-        bold: true
+  return [
+    new Paragraph({
+      text: card.title,
+      heading: HeadingLevel.HEADING_1,
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: '\n',
+          size: '14pt'
+        })
+      ]
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: 'Assigned To: ',
+          bold: true,
+          size: '14pt'
+        }),
+        new TextRun({
+          text: card.assignedTo.map(member => member.name ? member.name : member.username).join(', '),
+          size: '14pt'
+        })
+      ]
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: '\n',
+          size: '14pt'
+        })
+      ]
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: 'Labels: ',
+          bold: true,
+          size: '14pt'
+        }),
+        new TextRun({
+          text: card.labels.map(label => `${label.name ? label.name : '<Unnamed>'} (${label.color})`).join(', '),
+          size: '14pt'
+        })
+      ]
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: '\n',
+          size: '14pt'
+        })
+      ]
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: 'Description:',
+          size: '14pt',
+          bold: true
+        })
+      ]
+    }),
+    ...card.description.split('\n').map(descriptionPart => {
+      return new Paragraph({
+        children: [
+          new TextRun({
+            text: descriptionPart,
+            size: '14pt'
+          })
+        ]
       })
-    ]
-  })
+    }),
+    new Paragraph({
+      children: [
+        new TextRun({
+          text: '\n',
+          size: '14pt'
+        })
+      ]
+    })
+  ]
 }
 
 module.exports = { generateDocumentForSprintPlan }
