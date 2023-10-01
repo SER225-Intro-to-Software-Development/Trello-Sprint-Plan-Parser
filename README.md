2e954546552e35bfe5ad865b50a29a4df7f09d9f
a-r-t <alexthimineur@gmail.com>
2023-09-30 20:58:54 -0400

    updated instructions and added readme

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Trello Sprint Plan Parser
 
-This website can be accessed at [https://a-r-t.github.io/Trello-Sprint-Plan-Parser/]( https://a-r-t.github.io/Trello-Sprint-Plan-Parser/).
+This website can be accessed at [https://ser225-intro-to-software-development.github.io/Trello-Sprint-Plan-Parser/](https://ser225-intro-to-software-development.github.io/Trello-Sprint-Plan-Parser/).
 
 ## What is this website for?
 
 This is a website for converting my students' Sprint Plans for their Quinnipiac's SER225 course from a Trello board into a document which can easily be submitted to Blackboard.
+
+The way the class works, teams must plan their Sprint out and create user stories on their Trello boards prior to getting the official "okay" to start working on it. I then require students to essentially submit a document containing "snapshot" of their Sprint user stories from their Trello board to Blackboard in order for me to have it on record -- since Trello boards will be updated as the Sprint goes on, I require this record in the case that either I or the team need to refer back to the original plan. Additionally, I use a grade on this Blackboard submission to mark that I have "officially" approved a team to move forward with their Sprint plan.
+
+...However, the one problem with this system is that my students essentially have to essentially copy/pasting or screenshotting all of their Trello tasks to a document, which is a waste of their valuable time. In order to make this easier on my students, I created this website that will automatically transform their Trello board Sprint tasks into a Word document that is ready to be submitted to Blackboard right out of the gate.
+
+## How to use this generator tool to convert a Trello board into a Sprint planning document
+
+Usage instructions are included right on the home page of the website underneath the tool itself. It essentially involves moving all new Sprint tasks to their own column, and then grabbing the Trello board's JSON representation (which is very easily obtained) -- from there, paste the information into the fields and hit the button to generate and download the Sprint planning Word document.
--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -2,29 +2,33 @@
 <div>
   <h2>Instructions</h2>
   <div class="step">
-    <p><b>Step 1:</b> Go to your team's Trello Board in your browser.</p>
+    <p><b>Step 1:</b> Place all new Sprint tasks in your Trello board under the same column. In the below example, all upcoming Sprint tasks are placed under a column named "Sprint Backlog":</p>
+    <img class="border" src="@/assets/images/sprint-backlog-column.png"/>
+  </div>
+  <div class="step">
+    <p><b>Step 2:</b> Go to your team's Trello Board in your browser.</p>
     <img class="border" src="@/assets/images/trello-address-bar-url.png"/>
   </div>
   <div class="step">
-    <p><b>Step 2:</b> Go to the browser's address bar where you have your Trello board open, add ".json" on to the end of the url, and then hit enter</p>
+    <p><b>Step 3:</b> Go to the browser's address bar where you have your Trello board open, add ".json" on to the end of the url, and then hit enter</p>
     <img class="border" src="@/assets/images/trello-address-bar-url-json.png"/>
     <p>You should now see a page with one very large JSON string:</p>
     <img class="border" src="@/assets/images/trello-json.png"/>
   </div>
   <div class="step">
-    <p><b>Step 3:</b> Copy the entire JSON string.</p>
+    <p><b>Step 4:</b> Copy the entire JSON string.</p>
     <img class="border" src="@/assets/images/trello-json-copy.png"/>
   </div>
   <div class="step">
-    <p><b>Step 4:</b> Paste the JSON string into the 'Trello JSON' text field.</p>
+    <p><b>Step 5:</b> Paste the JSON string into the 'Trello JSON' text field.</p>
     <img class="border" src="@/assets/images/trello-json-field.png"/>
   </div>
   <div class="step">
-    <p><b>Step 5:</b> Place all new Sprint tasks in your Trello board under the same column, and then put the name of that column in the 'Sprint Plan Column Name' text field.</p>
+    <p><b>Step 6:</b> Put the name of the column that contains all of your team's upcoming Sprint tasks (per Step 1) in the 'Sprint Plan Column Name' text field.</p>
     <img class="border" src="@/assets/images/column-name-field.png"/>
   </div>
   <div class="step">
-    <p><b>Step 6:</b> Click the 'Convert to Document' button and a Sprint Planning document will be created for you to download (.docx file). This document can be submitted right to Blackboard. I recommend opening it up and just ensuring this tool generated the document correctly before submitting.</p>
+    <p><b>Step 7:</b> Click the 'Convert to Document' button and a Sprint Planning document will be created for you to download (.docx file). This document can be submitted right to Blackboard. I recommend opening it up and just ensuring this tool generated the document correctly before submitting.</p>
   </div>
 </div>
 </template>
